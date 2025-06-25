b7998b899d6dadc701759bf9e9e05d90320ad62d
estaiazep <froynik@gmail.com>
2025-06-25 17:42:07 +0500

    Update CanvasMines-3.js

--- bas/nayriz/CanvasMines-3.js
+++ bas/nayriz/CanvasMines-3.js
@@ -29,9 +29,9 @@ window.addEventListener("load", resizeCanvas)
 
 // Загрузка изображений
 const rectangleImg = new Image()
-rectangleImg.src = "RectangleMines.svg"
+rectangleImg.src = "../bas/nayriz/resource/RectangleMines.svg"
 const starImg = new Image()
-starImg.src = "StarMines.svg"
+starImg.src = "../bas/nayriz/resource/StarMines.svg"
 
 // Кастомизируемые переменные
 const gridSize = 5 // Размер сетки (количество прямоугольников в ряду/столбце)
